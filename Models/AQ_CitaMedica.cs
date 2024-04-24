f4d284e681ded9df74fa9d188b0441aed685de82
Anahi Quezada <anahi.quezada@udla.edu.ec>
2024-04-24 10:53:30 -0500

    Creacion de la clase

---
+++ Models/AQ_CitaMedica.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AnahiQuezada_Examen1P.Models
+{
+    public class AQ_CitaMedica
+    {
+        public int AQ_Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string? AQ_Nombre { get; set; }
+        public bool AQ_Urgente { get; set; }
+        [Range(10, 9999.99)]
+        //[VerificarRango]
+        public decimal AQ_Precio { get; set; }
+    }
+}
+
+/*public class VerificarRango : Verificattion
+    {
+
+    }*/
